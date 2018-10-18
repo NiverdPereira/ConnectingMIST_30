f5368316a04d26994813247da46340ef688a2f22
RizonSunny <rizon.sunny@gmail.com>
2018-10-18 07:35:59 +0600

    login bug fixed + dialog box on notice and newsfeed.

--- app/src/main/java/e/par/connectingmist_30/HomeActivity.java
+++ app/src/main/java/e/par/connectingmist_30/HomeActivity.java
@@ -109,7 +109,7 @@ public class HomeActivity extends AppCompatActivity {
                         startActivity(hm);
                         break;
                     case R.id.logout:
-                        mPreferences = getSharedPreferences("User", MODE_PRIVATE);
+                        mPreferences = getSharedPreferences("Session", MODE_PRIVATE);
                         SharedPreferences.Editor editor = mPreferences.edit();
                         editor.clear();   // This will delete all your preferences, check how to delete just one
                         editor.commit();
@@ -125,7 +125,10 @@ public class HomeActivity extends AppCompatActivity {
     }
     @Override
     public void onBackPressed() {
-
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
     protected void goToInsertion()
     {
